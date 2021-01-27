2a3404f2358fb7bfcaf6be07da124cab62f84829
Raul Moscoso <raul.moscoso84@gmail.com>
2021-01-27 00:18:15 -0600

    Boton de importar y exportar

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-class CategoriaAdmin(admin.ModelAdmin):
+class CategoriaResource(resources.ModelResource):
+    class Meta:
+        model = Categoria
+
+class AutorResource(resources.ModelResource):
+    class Meta:
+        model = Autor
+
+class CategoriaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['nombre']
     list_display = ('nombre', 'status', 'created')
+    resource_class = CategoriaResource
 
-class AutorAdmin(admin.ModelAdmin):
+class AutorAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['nombres', 'apellidos']
     list_display = ('nombres', 'apellidos', 'correo', 'created')
+    resource_class = AutorResource
 
 
 # Register your models here.
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.blog'
+    'apps.blog',
+    'import_export',
 ]
 
 MIDDLEWARE = [
